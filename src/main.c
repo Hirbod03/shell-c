e4acce37a0039578891345a53c803bc3384b14f2
Hirbod <57376297+Hirbod03@users.noreply.github.com>
2025-12-02 09:26:08 -0500

    REPL - attempt 1

--- src/main.c
+++ src/main.c
@@ -6,18 +6,25 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-
   // Captures the user's command in the "command" variable
   char command[1024];
-  fgets(command, sizeof(command), stdin);
 
-  // Remove the trailing newline
-  command[strcspn(command, "\n")] = '\0';
+  // Infinite read-execute loop
+  while (1) {
+    // Prompt
+    printf("$ ");
 
-  // Prints "<command>: command not found" msg
-  printf("%s: command not found\n", command);
+    // Read input; exit loop on EOF
+    if (fgets(command, sizeof(command), stdin) == NULL) {
+      break;
+    }
+
+    // Remove the trailing newline
+    command[strcspn(command, "\n")] = '\0';
+
+    // Prints "<command>: command not found" msg
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }
