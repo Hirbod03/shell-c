99e0a7a902dbb0614421c5da5f7091f9670a9b46
Hirbod <57376297+Hirbod03@users.noreply.github.com>
2025-12-02 09:19:17 -0500

    command not found - first attempt

--- src/main.c
+++ src/main.c
@@ -8,5 +8,11 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
 
+  // Captures the user's command in the "command" variable
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  printf("\n %c: command not found", command);
+
   return 0;
 }
